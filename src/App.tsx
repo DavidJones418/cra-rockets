2b190442fcb84ac4f58169c8e51cdbd74a0d1b42
David Jones <67751672+DavidJones418@users.noreply.github.com>
2021-04-26 00:22:55 +0100

    Style as mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import { useState } from 'react';
 import useSWR from 'swr';
-import './App.css';
-
-async function fetcher(url: string) {
-	const res = await fetch(url);
-	if (!res.ok) {
-		throw new Error(res.statusText);
-	}
-	return await res.json();
-}
+import styles from './App.module.css';
+import Launch from './Launch';
+import spaceXLogo from './spacex-logo.png';
 
 function getYear(launch: { date_unix: number }) {
 	return new Date(launch.date_unix * 1000).getUTCFullYear();
 }
 
 function App() {
-	const { data: launches, mutate } = useSWR<{ id: string; name: string; date_unix: number }[]>(
-		'https://api.spacexdata.com/v4/launches',
-		fetcher,
-	);
+	const { data: launches, mutate } = useSWR<
+		{ id: string; name: string; date_unix: number; flight_number: number; rocket: string }[]
+	>('https://api.spacexdata.com/v4/launches', (url) => fetch(url).then((res) => res.json()));
 
 	const launchYears = new Set(launches?.map(getYear));
 
@@ -27,10 +20,22 @@ function App() {
 	const [sortDirection, setSortDirection] = useState(1);
 
 	return (
-		<div className="App">
-			<header className="App-header">
-				<button onClick={() => mutate()}>Reload</button>
+		<main className={styles.App}>
+			<header className={styles.header}>
+				<h1 className={styles.heading}>
+					<img className={styles.headingLogo} src={spaceXLogo} alt="SpaceX" />
+					Launches
+				</h1>
+				<div>
+					<button className={styles.reloadButton} onClick={() => mutate()}>
+						Reload Data
+					</button>
+				</div>
+			</header>
+
+			<div className={styles.controls}>
 				<select
+					className={styles.filterSelect}
 					onChange={(ev) => setFilterYear(Number(ev.currentTarget.value))}
 					data-testid="filterByYear"
 				>
@@ -41,20 +46,23 @@ function App() {
 						</option>
 					))}
 				</select>
-				{sortDirection > 0 && <button onClick={() => setSortDirection(-1)}>Sort Descending</button>}
-				{sortDirection < 0 && <button onClick={() => setSortDirection(+1)}>Sort Ascending</button>}
-				<ol>
-					{launches
-						?.filter((launch) => !filterYear || getYear(launch) === filterYear)
-						.sort((a, b) => sortDirection * (a.date_unix - b.date_unix))
-						.map((launch, i) => (
-							<li key={launch.id} data-testid={`launch-${i}`}>
-								<span>{launch.name}</span> <span>({launch.date_unix})</span>
-							</li>
-						))}
-				</ol>
-			</header>
-		</div>
+
+				<button className={styles.sortButton} onClick={() => setSortDirection(-sortDirection)}>
+					{sortDirection > 0 ? 'Sort Descending' : 'Sort Ascending'}
+				</button>
+			</div>
+
+			<ol className={styles.launches}>
+				{launches
+					?.filter((launch) => !filterYear || getYear(launch) === filterYear)
+					.sort((a, b) => sortDirection * (a.date_unix - b.date_unix))
+					.map((launch, i) => (
+						<li key={launch.id} data-testid={`launch-${i}`}>
+							<Launch data={launch} />
+						</li>
+					))}
+			</ol>
+		</main>
 	);
 }
 
