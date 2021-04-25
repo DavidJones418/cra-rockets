5ff91b5f410694167a101b12c8dda6e14982a434
David Jones <DavidJones418@pm.me>
2021-04-25 20:09:42 +0100

    Sort all launches by date (ascending/descending) (#4)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,61 +1,77 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import { cache } from 'swr';
+import App from './App';
 
-// These entities contain the subset of properties of the real API used by the client.
 const mockLaunches = [
-	{ id: 'aaaaa', name: 'Mock First Launch', date_unix: Number(new Date('2006-01-01')) / 1000 },
-	{ id: 'bbbbb', name: 'Mock Second Launch', date_unix: Number(new Date('2007-01-01')) / 1000 },
-	{ id: 'ccccc', name: 'Mock Third Launch', date_unix: Number(new Date('2008-01-01')) / 1000 },
+	{ id: 'aaaaa', name: 'Mock 2006 Launch', date_unix: Number(new Date('2006-01-01')) / 1000 },
+	{ id: 'bbbbb', name: 'Mock 2007 Launch', date_unix: Number(new Date('2007-01-01')) / 1000 },
+	{ id: 'ccccc', name: 'Mock 2008 Launch', date_unix: Number(new Date('2008-01-01')) / 1000 },
 ];
 
 export const server = setupServer(
 	rest.get('https://api.spacexdata.com/v4/launches', (req, res, ctx) => {
-		return res(ctx.status(200), ctx.json(mockLaunches.slice(0, 2)));
+		return res(ctx.status(200), ctx.json(mockLaunches));
 	}),
 );
 
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
 
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests.
-afterEach(() => server.resetHandlers());
+afterEach(() => cache.clear());
 
 // Clean up after the tests are finished.
 afterAll(() => server.close());
 
 test('load the full list of SpaceX launches from the SpaceX API', async () => {
 	render(<App />);
-	expect(await screen.findByText('Mock First Launch')).toBeInTheDocument();
-	expect(await screen.findByText('Mock Second Launch')).toBeInTheDocument();
+	expect(await screen.findByText('Mock 2006 Launch')).toBeInTheDocument();
+	expect(await screen.findByText('Mock 2007 Launch')).toBeInTheDocument();
+	expect(await screen.findByText('Mock 2008 Launch')).toBeInTheDocument();
 });
 
 test('reload the data to see any new changes', async () => {
 	render(<App />);
+	// Wait for the initial load to populate the SWR cache.
+	await screen.findByText('Mock 2006 Launch');
 
-	// Wait for initial load.
-	await screen.findByText('Mock First Launch');
+	// Set a flag the next time the cache is updated when a refetch is requested.
+	// NOTE: this is a bit brittle, as the reload button really just invalidates
+	// the key in the SWR cache, relying on SWR to actually refetch the data.
+	let cacheUpdated = false;
+	const unsubscribe = cache.subscribe(() => {
+		cacheUpdated = true;
+		unsubscribe();
+	});
 
-	// Another launch is added after the initial load.
-	server.use(
-		rest.get('https://api.spacexdata.com/v4/launches', (req, res, ctx) => {
-			return res(ctx.status(200), ctx.json(mockLaunches.slice(0, 3)));
-		}),
-	);
+	fireEvent.click(screen.getByText('Reload'));
 
-	screen.getByText('Reload').click();
-	expect(await screen.findByText('Mock Third Launch')).toBeInTheDocument();
+	expect(cacheUpdated).toBe(true);
 });
 
 test('filter the launch list by year', async () => {
 	render(<App />);
-	const firstLaunch = await screen.findByText('Mock First Launch');
-	const secondLaunch = await screen.findByText('Mock Second Launch');
+	const firstLaunch = await screen.findByText('Mock 2006 Launch');
+	const secondLaunch = await screen.findByText('Mock 2007 Launch');
+	const thirdLaunch = await screen.findByText('Mock 2008 Launch');
 
-	fireEvent.change(screen.getByTestId('filterByYear'), { target: { value: '2006' } });
+	fireEvent.change(screen.getByTestId('filterByYear'), { target: { value: '2007' } });
 
-	expect(firstLaunch).toBeInTheDocument();
-	expect(secondLaunch).not.toBeInTheDocument();
+	expect(firstLaunch).not.toBeInTheDocument();
+	expect(secondLaunch).toBeInTheDocument();
+	expect(thirdLaunch).not.toBeInTheDocument();
+});
+
+test('sort all launches by date (ascending/descending)', async () => {
+	render(<App />);
+	expect(await screen.findByTestId('launch-0')).toHaveTextContent('Mock 2006 Launch');
+	expect(await screen.findByTestId('launch-1')).toHaveTextContent('Mock 2007 Launch');
+	expect(await screen.findByTestId('launch-2')).toHaveTextContent('Mock 2008 Launch');
+
+	fireEvent.click(screen.getByText('Sort Descending'));
+
+	expect(screen.getByTestId('launch-0')).toHaveTextContent('Mock 2008 Launch');
+	expect(screen.getByTestId('launch-1')).toHaveTextContent('Mock 2007 Launch');
+	expect(screen.getByTestId('launch-2')).toHaveTextContent('Mock 2006 Launch');
 });
